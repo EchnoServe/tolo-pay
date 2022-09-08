04fef9368d7698beeb0250668fdfd95e7d898979
yonasketema <yonask.kitaw@gmail.com>
2022-09-08 14:54:17 +0300

    add utils folder and color.js , media-queries.js files

---
+++ tolopay/src/Utils/colors.js
@@ -0,0 +1,6 @@
+export const primary = "#46833C";
+export const black = "#1D1F22";
+export const gray = "#f1f2f7";
+export const gray10 = "#f1f1f4";
+export const gray20 = "#e4e5e9";
+export const gray80 = "#333";
