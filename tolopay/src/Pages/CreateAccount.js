918b916237e6c9074f88299facdec174f4f68d3c
yonasketema <yonask.kitaw@gmail.com>
2022-09-08 14:42:08 +0300

    add createAccount components

--- tolopay/src/Pages/CreateAccount.js
+++ tolopay/src/Pages/CreateAccount.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const CreateAccount = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Create Account</h1>
+        <p>
+          Already have an account? <span>Login</span>
+        </p>
+        <div>
+          <button>Google</button>
+          <button>FaceBook</button>
+        </div>
+        <div>
+          <div></div>
+          <span>or</span>
+          <div></div>
+        </div>
+        <form>
+          <div>
+            <input type="text" placeholder="First name"></input>
+            <input type="text" placeholder="Last name"></input>
+          </div>
+          <input type="text" placeholder="name@gmail.com"></input>
+          <input type="password" placeholder="password"></input>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default CreateAccount;
