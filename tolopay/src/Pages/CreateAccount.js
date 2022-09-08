c3d172da68b3e879200e18539d17bc63e0f8c3e4
yonasketema <yonask.kitaw@gmail.com>
2022-09-08 16:01:39 +0300

    styling

--- tolopay/src/Pages/CreateAccount.js
+++ tolopay/src/Pages/CreateAccount.js
@@ -1,30 +1,135 @@
 import React from "react";
+import { gray, gray80, primary } from "../Utils/colors";
 
 const CreateAccount = () => {
   return (
     <>
-      <div>
-        <h1>Create Account</h1>
-        <p>
-          Already have an account? <span>Login</span>
-        </p>
-        <div>
-          <button>Google</button>
-          <button>FaceBook</button>
-        </div>
-        <div>
-          <div></div>
-          <span>or</span>
-          <div></div>
-        </div>
-        <form>
-          <div>
-            <input type="text" placeholder="First name"></input>
-            <input type="text" placeholder="Last name"></input>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <h1>Create Account</h1>
+          <p>
+            Already have an account? <u style={{ color: primary }}>Login</u>
+          </p>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "column",
+              gap: "0.5rem",
+            }}
+          >
+            <button
+              style={{
+                padding: "12px 10px",
+                border: `1px solid ${primary}`,
+                backgroundColor: "transparent",
+              }}
+            >
+              Sign up with Google
+            </button>
+            <button
+              style={{
+                padding: "12px 10px",
+                border: `1px solid ${primary}`,
+                backgroundColor: "transparent",
+              }}
+            >
+              Sign up with FaceBook
+            </button>
           </div>
-          <input type="text" placeholder="name@gmail.com"></input>
-          <input type="password" placeholder="password"></input>
-        </form>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "1rem",
+              width: "100%",
+            }}
+          >
+            <div
+              style={{
+                borderBottom: `1px solid ${primary}`,
+                width: "100%",
+              }}
+            ></div>
+            <span>or</span>
+            <div
+              style={{
+                borderBottom: `1px solid ${primary}`,
+                width: "100%",
+              }}
+            ></div>
+          </div>
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "1rem",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "1rem",
+              }}
+            >
+              <input
+                type="text"
+                placeholder="First name"
+                style={{
+                  padding: "12px 10px",
+                  border: `1px solid ${primary}`,
+                }}
+              ></input>
+              <input
+                type="text"
+                placeholder="Last name"
+                style={{
+                  padding: "12px 10px",
+                  border: `1px solid ${primary}`,
+                }}
+              ></input>
+            </div>
+            <input
+              type="text"
+              placeholder="name@gmail.com"
+              style={{
+                width: "100%",
+                padding: "12px 10px",
+                border: `1px solid ${primary}`,
+              }}
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              style={{
+                width: "100%",
+                padding: "12px 10px",
+                border: `1px solid ${primary}`,
+              }}
+            ></input>
+          </form>
+        </div>
+        <div>Image</div>
       </div>
     </>
   );
