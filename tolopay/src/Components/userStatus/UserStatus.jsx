bf15537972c26f7bf34f56700043cf0ab2924019
Bisrat Girma <bisratx@gmail.com>
2022-09-16 02:59:41 +0300

    file setup

---
+++ tolopay/src/Components/userStatus/UserStatus.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserStatus = () => {
+  return (
+    <div>UserStatus</div>
+  )
+}
+
+export default UserStatus
---
+++ tolopay/src/Pages/userProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserInfo from '../../Components/userInfo/UserInfo'
+import UserStatus from '../../Components/userStatus/UserStatus'
+
+const UserProfile = () => {
+  return (
+    <div>
+        <UserInfo />
+        <UserStatus />
+    </div>
+  )
+}
+
+export default UserProfile
