0aad9f5645290763aab53712e8b7511d48f72e5d
yonasketema <yonask.kitaw@gmail.com>
2022-09-16 21:09:43 +0300

    add qr page

--- tolopay/src/index.js
+++ tolopay/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import CreateAccount from "./Pages/CreateAccount";
+import CreateAccount from "./Pages/createAccount/CreateAccount";
+import Qr from "./Pages/qr/Qr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <CreateAccount />
+    <Qr />
   </React.StrictMode>
 );
 
