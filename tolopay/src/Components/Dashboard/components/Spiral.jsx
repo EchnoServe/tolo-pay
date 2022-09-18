7d2bff9510f13487fce2fae104a9156b016b6e41
birtukan <birtukandegu2127@gmail.com>
2022-09-18 17:59:35 +0300

    modified dashboard

--- tolopay/src/Components/Dashboard/components/Spiral.jsx
+++ tolopay/src/Components/Dashboard/components/Spiral.jsx
@@ -1,73 +1,73 @@
 import React from 'react'
 import { RadialBar } from 'recharts'
-// import { Legend } from 'recharts'
+import { Legend } from 'recharts'
 import { Tooltip } from 'recharts'
 import { RadialBarChart } from 'recharts'
 import styled from 'styled-components'
 
 const data = [
   {
-    "name": "18-24",
+    "name": "Sunday",
     "uv": 31.47,
-    "pv": 2400,
     "fill": "#203a21"
   },
   {
-    "name": "25-29",
+    "name": "Monday",
     "uv": 26.69,
-    "pv": 4567,
     "fill": "#28492a"
   },
   {
-    "name": "30-34",
+    "name": "Tuesday",
     "uv": -15.69,
-    "pv": 1398,
     "fill": "#215732"
   },
   {
-    "name": "35-39",
+    "name": "Wednsday",
     "uv": 8.22,
-    "pv": 9800,
     "fill": "#37663a"
   },
   {
-    "name": "40-49",
+    "name": "Thursday",
     "uv": -8.63,
-    "pv": 3908,
     "fill": "#66a266"
   },
   {
-    "name": "50+",
+    "name": "Friday",
     "uv": -2.63,
-    "pv": 4800,
     "fill": "#80b280"
   },
   {
-    "name": "unknow",
+    "name": "Saturday",
     "uv": 6.67,
-    "pv": 4800,
     "fill": "#99c199"
   }
 ]
 
+const style = {
+	top: 90,
+	left: 350,
+	lineHeight: '24px',
+};
 
 const Spiral = () => {
   return (
     <Div className='spiral'>
       <RadialBarChart
-        margin={{ top: 5, right: 5, bottom: 5, left: 5 }}
-        width={450} 
-        height={350} 
+        width={500} 
+        height={300} 
+        cx={150}
+        cy={180}
         innerRadius="20%" 
-        outerRadius="100%" 
+        outerRadius="130%"
+        barSize={500} 
         data={data} 
         startAngle={135} 
         endAngle={-210}
-        barCategoryGap="1%"
+        barCategoryGap="5%"
         
 >
         <RadialBar minAngle={15} label={{ fill: '#000', position: 'insideStart' }} background clockWise={true} dataKey='uv' />
-        {/* <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" /> */}
+        <Legend iconSize={30} width={120} height={140} layout='vertical' verticalAlign='middle' wrapperStyle={style}/>
         <Tooltip />
       </RadialBarChart>
     </Div>
@@ -75,8 +75,8 @@ const Spiral = () => {
 }
 
 const Div = styled.div`
-//box-shadow: 0 20px 50px #46833c; 
-margin-top: 4rem;
+// box-shadow: 0 20px 50px #46833c; 
+margin-top: 5rem;
 .spiral {
   display: flex;
   flex-direction: column;
