b445574458c9343b12157c977c059f2f18024088
Melat <melatkassaye13@gmail.com>
2022-09-19 10:26:23 +0300

    fixed issues with the styles

--- tolopay/src/App.js
+++ tolopay/src/App.js
@@ -1,28 +1,45 @@
 import "./App.css";
+import React, { useState } from "react";
 import LoginPage from "./Pages/login_page/LoginPage";
+import Modal from "./Components/confirmation_component/Transfer"
 
 function App() {
-  return (<div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#46833C",
-        color: "#fff",
-        gap: "3rem",
-      }}
-    >
-      <h1
-        style={{
-          fontSize: "10rem",
+  const [modalOpen, setModalOpen] = useState(false);
+  return(
+  <div>
+    <button
+  style={{align:"center"}}
+    className="openModalBtn"
+        onClick={() => {
+          setModalOpen(true);
         }}
       >
-        Tolo-pay
-      </h1>
-      <p>Pay Anything Anywhere Easily</p>
-    </div>
+        Open
+
+    </button>
+    {modalOpen && <Modal setOpenModal={setModalOpen} />}
+  </div>
+  // return (<div
+  //     style={{
+  //       display: "flex",
+  //       flexDirection: "column",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //       height: "100vh",
+  //       backgroundColor: "#46833C",
+  //       color: "#fff",
+  //       gap: "3rem",
+  //     }}
+  //   >
+  //     <h1
+  //       style={{
+  //         fontSize: "10rem",
+  //       }}
+  //     >
+  //       Tolo-pay
+  //     </h1>
+  //     <p>Pay Anything Anywhere Easily</p>
+  //   </div>
   );
 }
 
