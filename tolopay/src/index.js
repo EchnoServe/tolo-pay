014343ec4a63a67f3845a7f3fd6b52e6146caf28
Bisrat Girma <30285329+bisratgirma@users.noreply.github.com>
2022-09-19 16:29:29 +0300

    Update index.js
    Co-authored-by: YonasKetema <Yonask.kitaw@gmail.com>

--- tolopay/src/index.js
+++ tolopay/src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import CreateAccount from "./Pages/createAccount/CreateAccount";
-import Qr from "./Pages/qr/Qr";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CreateAccount />
-    <Qr />
+    <App />
   </React.StrictMode>
 );
 
