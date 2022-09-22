fb959f5cd3a6e17494b7bd8d3a65d4e55de85b16
Melat <melatkassaye13@gmail.com>
2022-09-22 09:59:33 +0300

    integrated with backend

--- tolopay/src/Pages/createAccount/CreateAccount.js
+++ tolopay/src/Pages/createAccount/CreateAccount.js
@@ -4,21 +4,46 @@ import { useForm } from "react-hook-form";
 import React, { useState } from "react";
 import "../../Components/signup_validation/signup.css";
 import { Formik } from 'formik';
+import axios from 'axios';
+import api from "../../api";
+import LoginPage from "../login_page/LoginPage";
  
 
 const CreateAccount = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [error, setError] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const {name,email,phoneNumber,password,passwordConfirm} = e.target.elements;
+    console.log({name,email,phoneNumber,password,passwordConfirm})
+    setError(false);
+    try {
+      const res = await axios.post("http://localhost:8000/api/v1/users/signup", {
+        name:name.value,
+        email: email.value,
+        phoneNumber: phoneNumber.value,
+        password:password.value,
+        passwordConfirm:passwordConfirm.value,
+       
+      });console.log(res.data);
+    // res.data&&window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
   const {
     register,
-    handleSubmit,
+    handleSubmits,
     formState: { errors },
     reset,
     trigger,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    reset();
-  };
   return (
     <>
       <header className="flex-c-r header">
@@ -38,7 +63,7 @@ const CreateAccount = () => {
         <div className="flex-c-c form">
           <h1>Create Account</h1>
           <p>
-            Already have an account? <u style={{ color: primary }}>Login</u>
+            Already have an account? <u style={{ color: '#404550' }}>Login</u>
           </p>
           <div className="flex-c-c signup_btns">
             <div className="flex-c-r p-12-10 btn">
@@ -71,43 +96,32 @@ const CreateAccount = () => {
           
        
 
-          <form className="flex-c-c  form_container" onSubmit={handleSubmit(onSubmit)}>
+          <form className="flex-c-c  form_container" onSubmit={handleSubmit}>
              <input
+                id="name"
                 type="text"
-                placeholder="first name"
-                className={`form-control ${errors.firstName && "invalid"}`}
-                {...register("firstName", { required: "FirstName is Required" ,
+                placeholder="user name"
+                onChange={(e) => setName(e.target.value)}
+                className={`form-control ${errors.userName && "invalid"}`}
+                {...register("userName", { required: "UserName is Required" ,
                 pattern: {
                 value: /^[A-Za-z]/,
                 message: "Invalid input name must only contain letters",
                 }})}
                 onKeyUp={() => {
-                  trigger("firstName");
+                  trigger("userName");
                 }}
               ></input>
-              {errors.firstName && (
-                <small className="text-danger">{errors.firstName.message}</small>
+              {errors.userName && (
+                <small className="text-danger">{errors.userName.message}</small>
               )}
-              <input
-                type="text"
-                placeholder="last name"
-                className={`form-control ${errors.lastName && "invalid"}`}
-                {...register("lastName", { required: "LastName is Required" ,
-                pattern: {
-                value: /^[A-Za-z]/,
-                message: "Invalid input name must only contain letters",
-                }})}
-                onKeyUp={() => {
-                  trigger("lastName");
-                }}
-              ></input>
-              {errors.lastName && (
-                <small className="text-danger">{errors.lastName.message}</small>
-              )}
+      
           
           <input
+              id="email"
               type="email"
               placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
                 className={`form-control ${errors.email && "invalid"}`}
                 {...register("email", { required: "Email address is Required" ,
                 pattern: {
@@ -121,9 +135,32 @@ const CreateAccount = () => {
               {errors.email && (
                 <small className="text-danger">{errors.email.message}</small>
               )}
+              
+           <input
+              id="phoneNumber"
+              type="number"
+              placeholder="phone Number "
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className={`form-control ${errors.phone && "invalid"}`}
+                {...register("phone", { required: "Phone  number is Required",
+                pattern: {
+                  value: /^\s*(?:\+?(\d[09]))[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{2})(?: *x(\d+))?\s*$/,
+                  message: "Invalid phone number",
+                },
+               })}
+               onKeyUp={() => {
+                trigger("phone");
+              }}
+            ></input>
+              {errors.phone && (
+                <small className="text-danger">{errors.phone.message}</small>
+              )}
+
             <input
+              id="password"
               type="password"
               placeholder="password"
+              onChange={(e) => setPassword(e.target.value)} 
               className={`form-control ${errors.message && "invalid"}`}
                 {...register("message", { required: "Password is Required",
                 minLength: {
@@ -143,8 +180,10 @@ const CreateAccount = () => {
                 <small className="text-danger">{errors.message.message}</small>
               )}
                 <input
+              id="passwordConfirm"
               type="password"
               placeholder="confirm password"
+              onChange={(e) => setPasswordConfirm(e.target.value)} 
               className={`form-control ${errors.confirmPassword && "invalid"}`}
                 {...register("confirmPassword", { required: "Password is Required",
                })}
