196ea85d340c2d9662c7b9a6d2f8e12edce0dc03
yonasketema <yonask.kitaw@gmail.com>
2022-09-23 09:40:00 +0300

    feat:transaction

--- tolopay/src/Components/walletToWallet/WalletToWallet.jsx
+++ tolopay/src/Components/walletToWallet/WalletToWallet.jsx
@@ -1,160 +1,181 @@
-import { useState } from "react"
-import styled from 'styled-components'
-
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
 
 const WalletToWallet = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
 
-    const [passwordShown, setPasswordShown] = useState(false);
+  const [values, setValues] = useState({
+    phoneNumber: "",
+    amount: "",
+    remark: "",
+    password: "",
+  });
 
+  const handlePhoneNumber = (event) => {
+    setValues({ ...values, phoneNumber: event.target.value });
+  };
 
-    const [values, setValues] = useState({
-        customerWalletNumber:"",
-        amount:"",
-        remark:"",
-        password:""
-    });
+  const handleAmount = (event) => {
+    setValues({ ...values, amount: event.target.value });
+  };
 
-const handleCustomerWalletNumber = (event) =>{
-    setValues({...values, customerWalletNumber: event.target.value})
-}
+  const handlePassword = (event) => {
+    setValues({ ...values, password: event.target.value });
+  };
 
-const handleAmount = (event) =>{
-    setValues({...values, amount: event.target.value})
-}
+  async function makePostRequest(values) {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/api/v1/transaction/transfer",
+        values,
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMmMwYmU0YWY1MzdhZDRjZmJmZGY1MCIsImlhdCI6MTY2MzgzMTM4NSwiZXhwIjoxNjcxNjA3Mzg1fQ.3UMcrl26NP8YVnUvt2_ZVaU2GCAn6YPUHsjYlIUD5LQ",
+          },
+        }
+      );
+    } catch (err) {
+      console.log("[Login.js] makePostRequest", err);
 
-const handlePassword = (event) =>{
-    setValues({...values, password: event.target.value})
-}
+      ///some thing set to be true
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(values);
+    makePostRequest(values);
+  };
 
   return (
     <Section>
-    <div className="walletToWallet">
-        <form className="moneyTransfer">
-            <h3 >wallet to wallet</h3>
-            <input
-                onChange={handleCustomerWalletNumber}
-                value={values.customerWalletNumber} 
-                className="form-field" 
-                placeholder="Customer wallet number" 
-                name="customerWalletNumber" />
-            <input 
-                onChange={handleAmount}
-                value={values.amount}
-                className="form-field" 
-                placeholder="Amount" 
-                name="amount" />
-            
-            <select className="option" id="remark">
-                 <option value="" selected hidden>Remark</option>
-                 <option value="option 1">option1</option>
-                 <option value="option 2">option2</option>
-                 <option value="option 3">option3</option>
+      <div className="walletToWallet">
+        <form className="moneyTransfer" onSubmit={handleSubmit}>
+          <h3>wallet to wallet</h3>
+          <input
+            onChange={handlePhoneNumber}
+            value={values.phoneNumber}
+            className="form-field"
+            placeholder="phone number"
+            name="phoneNumber"
+          />
+          <input
+            onChange={handleAmount}
+            value={values.amount}
+            className="form-field"
+            placeholder="Amount"
+            name="amount"
+          />
 
-            </select> 
-               
-            <input 
-                onChange={handlePassword}
-                value={values.password}
-                className="form-field" 
-                placeholder="Password"
-                // type="password" 
-                type={passwordShown ? "text" : "password"}
-                name="password" 
-                required/>
-            <button
-                className="btn"
-                type="submit">Transfer</button>
+          <select className="option" id="remark">
+            <option value="">Remark</option>
+            <option value="option 1">option1</option>
+            <option value="option 2">option2</option>
+            <option value="option 3">option3</option>
+          </select>
+
+          <input
+            onChange={handlePassword}
+            value={values.password}
+            className="form-field"
+            placeholder="Password"
+            // type="password"
+            type={passwordShown ? "text" : "password"}
+            name="password"
+            required
+          />
+          <button className="btn" type="submit">
+            Transfer
+          </button>
         </form>
-    </div>
+      </div>
     </Section>
-  )
-}
+  );
+};
 
 const Section = styled.section`
-.walletToWallet{
+  .walletToWallet {
     display: flex;
     min-height: 100vh;
     justify-content: center;
     align-items: center;
     margin-top: 10px;
-  
+
     .moneyTransfer {
       display: flex;
       flex-direction: column;
       justify-content: space-evenly;
       padding: 10px;
-  
-      h3{
-          text-align: center;
-          text-decoration: 2.5px underline #46833c;
-          text-underline-offset: 8px;
-          
+
+      h3 {
+        text-align: center;
+        text-decoration: 2.5px underline #46833c;
+        text-underline-offset: 8px;
       }
-  
+
       .option {
-          color: gray;
+        color: gray;
       }
       option {
-          border: 2px;
-          color: #000000;
+        border: 2px;
+        color: #000000;
       }
-      
     }
-    
-    .option{
+
+    .option {
       background-color: #fff;
       margin: 10px 0 10px 0;
       padding: 15px;
       font-size: 16px;
       border: 2px solid #9dc297;
     }
-  
-    .option:hover{
+
+    .option:hover {
       background-color: #fff;
       margin: 10px 0 10px 0;
       padding: 15px;
       font-size: 16px;
       border: 2px solid #46833c;
     }
-    
-    .form-field{
+
+    .form-field {
       margin: 10px 0 10px 0;
       padding: 15px;
       font-size: 16px;
       border-style: solid;
       border-color: #9dc297;
     }
-  
-    .form-field:hover{
+
+    .form-field:hover {
       margin: 10px 0 10px 0;
       padding: 15px;
       font-size: 16px;
       border-style: solid;
       border-color: #46833c;
     }
-  
-    .btn{
+
+    .btn {
       margin: 10px 0 10px 0;
       padding: 15px;
       font-size: 16px;
       border: none;
     }
-    
-  
-    
+
     input {
       background: white;
       padding: 15px;
       margin: 10px 0px;
       width: 400px;
     }
-    
+
     .pass-wrapper {
       position: relative;
       display: flex;
       margin-bottom: 14px;
     }
-    
+
     i {
       position: absolute;
       top: 38%;
@@ -164,26 +185,22 @@ const Section = styled.section`
       color: #00fcb6;
       cursor: pointer;
     }
-    
+
     button {
       background: #46833c;
       color: white;
       cursor: pointer;
       background-image: linear-gradient(to bottom right, #46833c, #9fcb98);
     }
-    
-    button:hover{
+
+    button:hover {
       background-image: linear-gradient(to bottom right, #285f1f, #7ab671);
     }
-   
-    
+
     button:disabled {
       cursor: default;
     }
   }
-  
-  
-  
 `;
 
-export default WalletToWallet
+export default WalletToWallet;
