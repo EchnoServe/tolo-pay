40781644349459de46f70b95593aea81c02b62dd
sumeya <isume295@gmail.com>
2022-10-17 22:52:16 -0700

    sidebar added

--- tolopay/src/App.js
+++ tolopay/src/App.js
@@ -1,9 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from "./Pages/home_page/Home";
 import Layout from './Components/admin_dashboard/Layout';
-import GlobalStyle from './GlobalStyle';
-import Profile from './Components/profilePage_component/Profile';
 import Try from './Components/Try/Try';
  
 
@@ -13,7 +10,7 @@ function App() {
        
       <Layout>
       <Routes>
-        <Route path="/qr" exact element = {<Home/>}/>
+        
         <Route path="/profile" exact element = {<Try/>}/>
 
       </Routes>
--- tolopay/src/Components/profilePage_component/ProfileUpdate.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ProfileUpdate() {
-  return (
-    <div>ProfileUpdate</div>
-  )
-}
