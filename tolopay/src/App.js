846d7a2639f9766a91302a840736afc15c40174d
Melat <melatkassaye13@gmail.com>
2022-10-19 00:26:03 +0300

    removed comments

--- tolopay/src/App.js
+++ tolopay/src/App.js
@@ -1,5 +1,5 @@
 
-import LoginPage from "./Pages/login_page/LoginPage";
+
 import Budget  from "./Pages/budget_page/Budget";
 import Transfer from "./Pages/transfer/Transfer";
 import WallettoWallet from  "./Components/walletToWallet/WalletToWallet"
@@ -8,25 +8,6 @@ function App() {
   return (<div>
 <Transfer/>
   </div>
-    //   style={{
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //     height: "100vh",
-    //     backgroundColor: "#46833C",
-    //     color: "#fff",
-    //     gap: "3rem",
-    //   }}
-    // >
-    //   <h1
-    //     style={{
-    //       fontSize: "10rem",
-    //     }}
-    //   >
-    //     Tolo-pay
-    //   </h1>
-    //   <p>Pay Anything Anywhere Easily</p>
    
   );
 }
