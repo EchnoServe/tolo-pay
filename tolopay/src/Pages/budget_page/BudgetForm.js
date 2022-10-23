c9ba6da5ba44b2389dd389c708419775ccf05bba
Melat <melatkassaye13@gmail.com>
2022-10-24 01:33:53 +0300

    edited styling

--- tolopay/src/Pages/budget_page/BudgetForm.js
+++ tolopay/src/Pages/budget_page/BudgetForm.js
@@ -17,6 +17,15 @@ const handleAmount = (event) =>{
 }
   return (
     <Section>
+        <Con>
+    <Container>
+      <SubContainer>
+        <SectionOne>
+
+          <ColumnTwo1>
+          <Setting >
+
+          <Section>
     <div className="budgetpage">
         <form className="addbudget">
             <h3 >Add Budget</h3>
@@ -38,9 +47,97 @@ const handleAmount = (event) =>{
         </form>
     </div>
     </Section>
+                </Setting> 
+             
+          </ColumnTwo1>
+        </SectionOne> 
+      </SubContainer>
+    </Container>
+    </Con>
+    </Section>
   )
 }
+const Container = styled.div`
+  width: 90%;
+  /* background: linear-gradient(to bottom right, white 0%, #e6e4ff 70%); */
+  /* border-bottom-right-radius: 2rem;
+  border-top-right-radius: 2rem; */
+  margin: 1rem 4rem 1rem 4rem;
+ 
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 1rem 0 0 0;
+  }
+`;
 
+const Con = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+/* background-color: black; */
+
+`;
+
+const SubContainer = styled.div`
+  /* margin: 0.5rem 0; */
+  height: 80%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: 100%;
+  }
+`;
+const SectionOne = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 40%;
+  gap: 2rem;
+  width: 100%;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    flex-direction: column;
+    align-items: center;
+    height: max-content;
+  }
+`;
+
+
+const ColumnTwo1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 115%;
+  width: 100%;
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: max-content;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+
+const Setting = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+  height: 100%;
+  background-color: rgba(58, 135, 190,0.1);
+  /* margin-bottom: 10px; */
+  padding: 10px;
+  transition: 0.4s ease-in-out;
+  color: #000;
+
+  
+  @media screen and (min-width: 320px) and (max-width: 1080px) {
+    height: max-content;
+    width: 80%;
+    margin-top: 1rem;
+  }
+`;
 const Section = styled.section`
 .budgetpage{
     display: flex;
