ec918a6ec6829b1c928ab54081c775afc4e10753
Bisrat Girma <bisratx@gmail.com>
2022-10-27 16:55:35 +0300

    app.js formating

--- tolopay/src/App.js
+++ tolopay/src/App.js
@@ -28,67 +28,49 @@ export const ThemeContext = React.createContext(null);
 
 const App = () => {
 
-  const { user,
-    //  dispatch ,
-     token } = useContext(Context);
+  const { user } = useContext(Context);
 
 
 
   const [theme, setTheme] = useState("light");
   const themeStyle = theme === "light" ? lightTheme : darkTheme;
 
-  console.log(user ,"tt",token)
-
   return (
- 
-<ThemeContext.Provider value={{ setTheme, theme }}>
-      <ThemeProvider theme={themeStyle}>
-        {/* <GlobalStyle />
-        <Helmet>
-          <title>tolopay</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-        </Helmet> */}
-        <>
-        <Router>
-
-          <Routes>
-
-            
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/login/success" element={<LoginSuccess />} />
-            <Route path="/password-reset" element={<EmailReset />} />
-            <Route path="/reset-password/:id/:token" element={<ResetPasswordPage />} />
-            <Route path="/" 
-                element = { 
-                    user ? 
-                    <Layout>
-                        <Chatbot />
-                        <BottomNavbar />
-                    </Layout> : 
-                    <Home />} >
-                    
-                    <Route index element={ <Dashboard /> } />
-                    <Route path="dashboard" element={ <Dashboard /> } />
-                    <Route path="transfer" element={ <WalletToWallet /> } />
-                    <Route path="budgetform" element={ <BudgetForm /> } />
-                    <Route path="planning" element={ <Budget /> } />
-                    <Route path="qr" element={ <QR /> } />
-                    <Route path="recieve" element={ <RecieveModal /> } />
-                    <Route path="send" element={ <Send />} />
-                    <Route path="profile" element={ <UserProfile /> } />
-                    <Route path="chatbot" element={ <Chatbot /> } />
-                    <Route path="message" element={<Message />} />
+      <ThemeContext.Provider value={{ setTheme, theme }}>
+          <ThemeProvider theme={themeStyle}>
+          
+            <Router>
+              <Routes>
+    
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/login/success" element={<LoginSuccess />} />
+                <Route path="/password-reset" element={<EmailReset />} />
+                <Route path="/reset-password/:id/:token" element={<ResetPasswordPage />} />
+                <Route path="/" 
+                    element = { 
+                        user ? 
+                        <Layout>
+                            <Chatbot />
+                            <BottomNavbar />
+                        </Layout> : 
+                        <Home />} >
+                        
+                        <Route index element={ <Dashboard /> } />
+                        <Route path="dashboard" element={ <Dashboard /> } />
+                        <Route path="transfer" element={ <WalletToWallet /> } />
+                        <Route path="budgetform" element={ <BudgetForm /> } />
+                        <Route path="planning" element={ <Budget /> } />
+                        <Route path="qr" element={ <QR /> } />
+                        <Route path="recieve" element={ <RecieveModal /> } />
+                        <Route path="send" element={ <Send />} />
+                        <Route path="profile" element={ <UserProfile /> } />
+                        <Route path="chatbot" element={ <Chatbot /> } />
+                        <Route path="message" element={<Message />} />
 
             </Route>
           </Routes>
         </Router>
-      </>
       </ThemeProvider>
     </ThemeContext.Provider>
   );
