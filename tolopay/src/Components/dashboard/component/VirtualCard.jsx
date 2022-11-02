cd6e6d90267ba46377c6d79adc2e029e8d973a93
Melat <melatkassaye13@gmail.com>
2022-11-02 18:52:37 +0300

    virtual card integrated

--- tolopay/src/Components/dashboard/component/VirtualCard.jsx
+++ tolopay/src/Components/dashboard/component/VirtualCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from "react";
+import React, { useContext } from "react";
 import styled from 'styled-components'
 import chip  from '../assets/chip.png'
 import visa from '../assets/visa.png'
